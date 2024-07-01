3f5a3bcf6bcf442175f58cf0966980dd4785571a
PatrickAngrezani <patricksantos99@outlook.com>
2024-07-01 12:39:26 -0300

    feat/start-playlists

--- src/routes/playlists.js
+++ src/routes/playlists.js
@@ -1,14 +1,23 @@
-const Track = require("../models/track");
+const { Track, Playlist } = require("../models/models");
 const express = require("express");
 const router = express.Router();
 
 router.post("/create-playlist", async (req, res) => {
-  const { name, tracks } = req.body;
-  // Salvar playlist no banco de dados
-  res.status(200).send("Playlist criada com sucesso");
+  const { name } = req.body;
+
+  try {
+    const newPlaylist = new Playlist({ name });
+    await newPlaylist.save();
+
+    res.status(201).send("Playlist created succesfully");
+  } catch (error) {
+    res.status(500).send("Error creating playlist:", error);
+  }
 });
 
 router.get("/playlists", async (req, res) => {
   // Recuperar playlists do banco de dados
   res.status(200).json(playlists);
 });
+
+module.exports = router;
