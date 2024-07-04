6d0f9f43efbf9e3159547c8206c362a70254eebe
PatrickAngrezani <patricksantos99@outlook.com>
2024-07-03 22:59:57 -0300

    fix: save in database

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const storage = multer.diskStorage({
     cb(null, "../uploads/");
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname + path.extname(file.originalname));
+    cb(null, file.originalname + path.extname(file.originalname.split(".")[1]));
   },
 });
 
